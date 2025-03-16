54b6676a825302babbfd723bee96ec449edc11e6
DavidMartinezFlores <dmartinezflores12@gmail.com>
2025-03-16 17:41:49 +0100

    FIRST UPDATE

---
+++ demoSpringBoot/mhbestiary/src/main/java/com/dmf/mhbestiary/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.dmf.mhbestiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author David Martinez Flores
+ */
+@SpringBootApplication
+public class DemoApplication
+{
+
+    /**
+     * Method main
+     *
+     * @param args the args
+     */
+	public static void main(String[] args)
+    {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
